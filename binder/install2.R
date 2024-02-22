94a5b245569c91120085ec2d8a3b89c896be6c4e
Serhat Cevikel <serhatcevikel@yahoo.com>
2024-02-22 03:52:57 +0300

    new packages

---
+++ binder/install2.R
@@ -0,0 +1,23 @@
+#!/bin/env R
+
+cranlist <- c('htm2txt','pdftools')
+
+githublist <- c('trinker/textreadr')
+
+## cran packages
+for (package in cranlist)
+{ 
+    if (!require(package, character.only = T, quietly = T))
+    {
+        install.packages(package)
+    }
+}
+
+for (package in githublist)
+{ 
+    if (!require(package, character.only = T, quietly = T))
+    {
+        devtools::install_github(package)
+    }
+}
+
