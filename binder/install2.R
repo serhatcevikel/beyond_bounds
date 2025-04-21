459b1db13fe5a406a94d4df4931707fc22c3bd69
Serhat Cevikel <serhatcevikel@yahoo.com>
2025-04-21 03:42:51 +0300

    new packages

--- binder/install2.R
+++ binder/install2.R
@@ -2,7 +2,12 @@
 
 cranlist <- c('AER','babynames','ckanr','dataverse','dbscan','dm','dslabs','fueleconomy','gapminder','ggstatsplot','here','htm2txt','janeaustenr','Lahman','leaflet.extras','leaflet.extras2','maps','mosaicData','pdftools','plm','renv','rjson','RKaggle','rsconnect','sentimentr','ucimlrepo','WDI','wooldridge','yarrr')
 
+cranlist2 <- c('ggstatsplot')
+
 githublist <- c('trinker/textreadr')
+
+cranlist2 <- intersect(cranlist2, cranlist)
+cranlist <- setdiff(cranlist, cranlist2)
 
 ## cran packages
 for (package in cranlist)
@@ -12,6 +17,15 @@ for (package in cranlist)
         install.packages(package, repo = "https://packagemanager.posit.co/cran/2025-04-19")
     }
 }
+
+for (package in cranlist2)
+{ 
+    if (!require(package, character.only = T, quietly = T))
+    {
+        install.packages(package)
+    }
+}
+
 
 for (package in githublist)
 { 
